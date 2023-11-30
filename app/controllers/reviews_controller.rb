0a88efb058d2c18649a11708ac3ebbecfa9e41a7
Eric Chan <hazzelnut@pm.me>
2023-11-29 19:06:11 -0800

    add review update, delete, and edit inside movie

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,51 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
+  end
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @review = @movie.reviews.new(review_params)
+
+    if @review.save
+      redirect_to movie_reviews_url(@movie), notice: 'Thanks for reviewing!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to movie_reviews_url(@movie), notice: 'Review successfully updated!'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    review = Review.find(params[:id])
+    review.destroy
+
+    redirect_to movie_reviews_url(@movie), status: :see_other, alert: 'Review successfully deleted!'
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :stars, :comment)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
