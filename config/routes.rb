769539dbcca03ea11324a921020bfd10e3f90e26
Eric Chan <hazzelnut@pm.me>
2023-12-02 17:14:59 -0800

    add signin form

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   resources :users
 
   get 'signup' => 'users#new'
+
+  resource :session, only: [:new, :create, :destroy]
+
+  get 'signin' => 'sessions#new'
 end
