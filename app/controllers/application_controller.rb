189000ca466a4aefdd5e607ca87496cd40767a9e
Eric Chan <hazzelnut@pm.me>
2023-12-11 20:45:31 -0800

    add auth; before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def current_user
+    # Saving info to an instance variable so when current_user is called
+    # 3 times in the _header, it doesn't have to make 3 queries.
+    # Instead, it will be cached in @current_user, and returned.
+    # The ||= operator runs the code on the right if @current_user doesn't have
+    # a value. Effectively, we run it once and return @current_user for
+    # subsequent calls.
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def require_signin
+    unless current_user
+      redirect_to new_session_url, alert: 'Please sign in first!'
+    end
+  end
 end
