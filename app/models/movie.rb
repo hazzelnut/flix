ca8a473cb64b2d0f9c93a97453a3d473d8d9b97e
Eric Chan <hazzelnut@pm.me>
2024-01-03 20:06:13 -0800

    add favorite resource

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ApplicationRecord
 
   has_many :reviews, dependent: :destroy
 
+  has_many :favorites, dependent: :destroy
+
   RATINGS = %w[G PG PG-13 R NC-17].freeze
 
   validates :title, :released_on, :duration, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+  has_many :favorites, dependent: :destroy
+
   has_secure_password
 
   validates :name, presence: true
